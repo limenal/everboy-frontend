eadeac289d897fefd6e2c13faab66112e594a9dd
Deltax2016 <deltaxussr@gmail.com>
2022-07-16 00:25:19 +0500

    it works

--- src/pages/EmulatorPage.jsx
+++ src/pages/EmulatorPage.jsx
@@ -1,19 +1,45 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useRef } from 'react'
 import { useSelector } from 'react-redux'
 import Game from '../components/Game'
 import getAllUserNfts from '../services/ntfIndexHelper'
 import everConn from '../connectors/everConnector'
+import spaceInvaders from '../files/SpaceInvaders.ch8'
 
 function EmulatorPage (props) {
 
     const walletAddress = useSelector((state) => state.accountAddress)
     const [userNfts, setUserNfts] = useState([])
+    const [timeOut, setTime] = useState(false);
+    const [file, setFile] = useState([]);
+    const [title, setTitle] = useState("Space Invaders");
+    const [keyPressed, setKey] = useState();
+    const inputRef = useRef(null);
+
     useEffect(() => {
         console.log(walletAddress)
         if (walletAddress) {
             getUserNfts()
         }
     }, [walletAddress])
+
+    const upload = (f) => {
+      if(!f)
+         return;
+      f.arrayBuffer().then(buffer => {  
+         let data = new Uint8Array(buffer);
+         setFile(data);
+      }).catch(e => console.log(e));    
+   }
+
+   const handleInput = () => {
+      inputRef.current?.click();
+   }
+
+   useEffect(() => {
+      (async function() {
+         upload(await fetch(spaceInvaders).then(r => r.blob()));   
+      })();
+   }, [])
 
     const getUserNfts = async () => {
         const [ever, account] = await everConn.connectToEverWallet()
@@ -29,7 +55,7 @@ function EmulatorPage (props) {
 
             </div>
             <div className='w-3/4 h-[650px] bg-white rounded-2xl'>
-
+                <Game file = {file} keyPressed = {keyPressed} setKey = {setKey} />
             </div>
           </div>
       </div>
