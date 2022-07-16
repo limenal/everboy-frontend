0cc197c6daceaeeea559c5e4f0a1a5ff058a0125
Limenal <lshrims@gmail.com>
2022-07-16 12:59:59 +0500

    fix typo

--- src/components/CartridgesList.jsx
+++ src/components/CartridgesList.jsx
@@ -1,12 +1,9 @@
 import Cartridge from './Cartridge'
  
 const CartridgesList = ({cartridgesArray}) => {
-    const array = [{
-        name: 'Game name',
-        pictureSrc: 'https://sun9-37.userapi.com/impf/yW3ENtmfWHl1t-B1g_2aXUu_u9YaLvpY-PEICw/NdfQL0XKCyM.jpg?size=353x323&quality=96&sign=a7bc1688765ac85f743667dc37a171e4&type=album'
-    }]
+
     return (
-        <div className='flext justify-start gap-x-10'>
+        <div className='flex justify-start gap-x-10'>
             {cartridgesArray.map((value, index) => {
                 return <Cartridge cartridgeInfo={value} key={index}/>
             })}
