f3221c51138e8b0e7132208dcd29435d68f45a48
ilau020203 <antip19052016@gmail.com>
2022-07-16 09:38:51 +0300

    add sale Service

--- src/pages/MarketPage.jsx
+++ src/pages/MarketPage.jsx
@@ -1,4 +1,48 @@
+
+import { useEffect, useState, useRef } from 'react'
+import { useSelector } from 'react-redux'
+import Game from '../components/Game'
+import getAllUserNfts from '../services/ntfIndexHelper'
+import {getAllSales} from '../services/salesService.js'
+import getNftInfo from '../services/nftService'
+import everConn from '../connectors/everConnector'
+import spaceInvaders from '../files/SpaceInvaders.ch8'
+import IBMLogo from '../files/IBMLogo.ch8'
+
+
+
+
 function MarketPage () {
+
+  const walletAddress = useSelector((state) => state.accountAddress)
+
+
+  useEffect(() => {
+      if (walletAddress) {
+        getUserSales()
+      }
+  }, [walletAddress])
+
+
+
+
+
+
+  const getUserSales = async () => {
+      const [ever, account] = await everConn.connectToEverWallet()
+
+      const something = await getAllSales(ever)
+      console.log("-------")
+      console.log(something)
+      console.log("-------")
+
+      
+      // const userNfts = await getAllUserNfts(walletAddress, ever)
+      // console.log(userNfts)
+      // const nftsInfo = await getNftInfo(userNfts, ever)
+      // console.log(nftsInfo)
+      // setUserNfts(userNfts)
+  }
     return (
       <div>
         <div className=''>
---
+++ src/services/salesService.js
@@ -0,0 +1,56 @@
+import { ProviderRpcClient, Address } from "everscale-inpage-provider";
+import axios from "axios";
+import Sale from "../abis/Sale.abi.json";
+
+const SALE_CODE_HASH =
+  "82cb7e5822f1e290654153168a948b13f129524d4b13bdf377daf09468f4d549";
+
+async function getAllSales(rpc) {
+  const hexString = SALE_CODE_HASH;
+  console.log(hexString);
+  const queryString = ` 
+    query { accounts (filter : {
+        code_hash :{eq : "${hexString}"}
+    })
+    {
+        id
+    }}
+    `;
+  const response = await axios({
+    url: "https://net.ton.dev/graphql",
+    method: "post",
+    data: {
+      query: queryString,
+    },
+  });
+  const promises = [];
+  response.data.data.accounts.forEach((item) => {
+    const contractAddress = new Address(item.id);
+    const contract = rpc.createContract(Sale, contractAddress);
+    promises.push(contract);
+  });
+  let contracts, contractCalls;
+  await Promise.allSettled(promises).then((results) => {
+    contracts = results;
+    contractCalls = results.map((contract) => {
+      return contract.value.methods
+        .getPrices({
+          answerId: 0,
+        })
+        .call();
+    });
+  });
+
+  const sale = [];
+  await Promise.allSettled(contractCalls).then((results) => {
+    results.forEach((item, index) =>
+      item.value.value0.forEach((v) => {
+
+        sale.push({ sale: contracts[index], json: v });
+      })
+    );
+  });
+  return sale;
+}
+
+export { getAllSales };
